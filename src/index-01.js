136cdf5d8e2a4c3bc9fd24a770047e9fba1deac5
EtErNaLiFe12 <49358321+EtErNaLiFe12@users.noreply.github.com>
2021-03-24 18:38:27 +0900

    index-01.js 내용 추가

--- src/index-01.js
+++ src/index-01.js
@@ -1,2 +1,36 @@
-let a = 5;
+// console.log('hello');
+/* let value = 1;
+   let value = 2; 
+   다음과 같이 같은 범위안에 같은 이름의 value가 선언이 될시에 에러 발생
+   단 다른 범위내에서는 같은 이름이어도 가능 */
+// ---------------------------------------------------------------
+// const a = 1;
+/* const는 상수값이므로 a = 2; or const a = 2;를 추가로 선언시에는 
+   에러 발생함 */
+// ---------------------------------------------------------------
+// let text = 'hello';
+// let name = '헬로우 자바스크립트';
+// ---------------------------------------------------------------
+// let good = true; boolean 값
+// let loading = false; boolean 값
+// let good = null; 값이 없음
+/* let something = undefined; 값이 지정되지 않음 
+   let criminal
+   console.log(criminal); 할 시 값은 undefined가 된다.*/
+// ---------------------------------------------------------------
+/* let a = 1;
+console.log(a++) result = 1;
+console.log(a) result = 2;
+console.log(++a) result = 3;  */
+// ---------------------------------------------------------------
+/* let a = 1;
+console.log(a--) result = 1;
+console.log(a) result = 0;
+console.log(--a) result = -1; */
+// ---------------------------------------------------------------
+let a = 1;
+a += 3;
+a -= 3;
+a *= 3;
+a /= 3;
 console.log(a);
