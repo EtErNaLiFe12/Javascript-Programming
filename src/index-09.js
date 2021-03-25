ee4006b34ac19b7bee7fe71e842834f259e43129
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-03-25 21:02:30 +0900

    index-09.js 반복문 - 일부 내용 추가

--- src/index-09.js
+++ src/index-09.js
@@ -93,6 +93,8 @@ console.log(result); */
 
 // --------------------------------------------
 
+// 숫자로 이루어진 배열이 주어졌을 때, 해당 숫자 배열안에 들어있는 숫자 중 3보다 큰 숫자로만 이루어진 배열을 새로 만들어서 반환해보세요.
+
 function biggerThanThree(numbers) {
    const array = [];
    for (let i = 0; i < numbers.length; i++) {
